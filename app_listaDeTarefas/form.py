2305dfce4553223d4f392f8cdd9222664b71504b
Gabriel Cheibe <gabriel.cheibe@otima.digital>
2024-03-06 10:53:24 -0300

    tabela e status na lista de tarefas

--- app_listaDeTarefas/form.py
+++ app_listaDeTarefas/form.py
@@ -4,4 +4,6 @@ from app_listaDeTarefas.models import ListadeTareda
 class ListadeTarefaForm(forms.ModelForm):
     class Meta:
         model = ListadeTareda
-        fields = '__all__'
+        fields = [
+            'tarefa',
+        ]
--- app_listaDeTarefas/models.py
+++ app_listaDeTarefas/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
+STATUS = [
+    ('fazer', 'A fazer'),
+    ('concluido', 'Concluido')
+]
+
 class ListadeTareda(models.Model):
     tarefa = models.CharField(max_length=255, verbose_name='Tarefa')
+    status = models.CharField(max_length=30, choices=STATUS, default='fazer')
 
     def __str__(self):
         return self.tarefa
